25e4f2a1c8b210769f3ba7e48708f8df353d611d
MikhailZamyslov <misha_zamyslov10@mail.ru>
2025-01-20 18:35:59 +0300

    Update ping_pong.py
    Законченная программа пинг понг

--- ping_pong.py
+++ ping_pong.py
@@ -22,9 +22,80 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
    # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+    
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+
+racket_image = 'racket.png'
+ball_image = 'tenis_ball.png'
+
+# игровая сцена
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+back = (200, 255, 255)
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player(racket_image, 30, 200, 50, 150, 4)
+racket2 = Player(racket_image, 520, 200, 50, 150, 4)
+
+ball = GameSprite(ball_image, 200, 200, 50, 50, 50)
+
+font.init()
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if not finish:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        window.fill(back)
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+
+        if ball.rect.y < 0 or ball.rect.y > win_height - 50:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+
+        elif ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2,(200,200))
+
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    
+    display.update()
+    clock.tick(FPS)
